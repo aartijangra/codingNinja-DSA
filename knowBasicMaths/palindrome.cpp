a31f61b57e40159ee05028742e046dbe75ce0d80
{Aarti} <aartijangra482@gmail.com>
2024-01-16 21:25:59 +0530

    basic maths

---
+++ knowBasicMaths/palindrome.cpp
@@ -0,0 +1,54 @@
+// Problem statement
+
+// Check whether a given number ’n’ is a palindrome number.
+
+// Note :
+// Palindrome numbers are the numbers that don't change when reversed.
+// You don’t need to print anything. Just implement the given function.
+// Example:
+// Input: 'n' = 51415
+// Output: true
+// Explanation: On reversing, 51415 gives 51415.
+// Detailed explanation ( Input/output format, Notes, Images )
+// Sample Input 1 :
+// 1032
+// Sample Output 1 :
+// false
+// Explanation Of Sample Input 1:
+// 1032, on being reversed, gives 2301, which is a totally different number.
+// Sample Input 2 :
+// 121
+// Sample Output 2 :
+// true
+// Explanation Of Sample Input 2:
+// 121, on being reversed, gives 121, which is the same.
+// Expected time complexity:
+// The expected time complexity is O(log(n)).
+// Constraints :
+// 1 <= n <= 10^9
+// Time Limit: 1 sec
+
+#include<iostream>
+using namespace std;
+bool palindrome(int n)
+{
+    int rev=0;
+    int temp=n;
+    while(n>0){
+        int r=n%10;
+        n=n/10;
+        rev= (rev*10)+r;
+    }
+    if(rev==temp){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    palindrome(n);
+    
+}
