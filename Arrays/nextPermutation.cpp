b7104df109379b367491a4cb67f342cfd9af38f8
{Aarti} <aartijangra482@gmail.com>
2024-02-26 22:54:11 +0530

    Arrays

---
+++ Arrays/nextPermutation.cpp
@@ -0,0 +1,71 @@
+// Problem statement
+// You are given an array ‘a’ of ‘n’ integers.
+// You have to return the lexicographically next to greater permutation.
+// Note:
+
+// If such a sequence is impossible, it must be rearranged in the lowest possible order.
+// Example:
+
+// Input: 'a' = [1, 3, 2]
+// Output: 2 1 3
+// Explanation: All the permutations of [1, 2, 3] are [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1], ]. Hence the next greater permutation of [1, 3, 2] is [2, 1, 3].
+// Detailed explanation ( Input/output format, Notes, Images )
+// Sample Input 1:
+// 3
+// 3 1 2
+// Sample Output 1:
+// 3 2 1
+// Explanation Of Sample Input 1:
+// Input:
+// A = [3, 1, 2]
+// Output:
+// 3 2 1
+
+// Explanation: All the permutations of [1, 2, 3] are [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1], ]. Hence the next greater permutation of [3, 1, 2] is [3, 2, 1].
+
+
+// Sample Input 2:
+// 3
+// 3 2 1
+// Sample Output 2:
+// 1 2 3
+// Constraints:
+// 1 <= n <= 100
+// 1 <= a[ i ] <= 100
+// Time Limit: 1 sec
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> nextGreaterPermutation(vector<int> &A) {
+    // Write your code here.
+    int n = A.size();
+    int ind = -1;
+    for(int i=n-2; i>=0; i--){
+        if(A[i]<A[i+1]){
+            ind=i;
+            break;
+        }
+    }
+    if(ind==-1){
+        reverse(A.begin() , A.end());
+        return A;
+    } 
+
+    for(int i=n-1; i>=ind; i--){
+        if(A[i] > A[ind]){
+            swap(A[i],A[ind]);
+            break;
+        }
+    }
+    //reverse(A,ind+1,n-1);
+    reverse(A.begin()+ind+1 , A.end());
+    return A;
+}
+
+int main(){
+    vector<int> A = {2,1,5,4,3,0,0};
+    vector<int> Ans = nextGreaterPermutation(A);
+    for(auto it: Ans){
+        cout<<it;
+    }
+    return 0;
+}
