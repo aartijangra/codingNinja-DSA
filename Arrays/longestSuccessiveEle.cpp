51ff7c231281341a2ccef11d167c4f1ccd4f42e5
aartijangra <aartijangra482@gmail.com>
2024-03-24 00:33:43 +0530

    arrays

--- Arrays/longestSuccessiveEle.cpp
+++ Arrays/longestSuccessiveEle.cpp
@@ -66,3 +66,57 @@ int main()
     cout << "The longest consecutive sequence is " << ans << "\n";
     return 0;
 }
+
+
+//better approach
+int longestSuccessiveElements(vector<int>&a) {
+    // Write your code here.
+    int n = a.size();
+    if(a.size() == 0) return 0;
+    sort(a.begin(),a.end());
+    int lastsmaller = INT_MIN;
+    int cnt = 0;
+    int longest=1;
+    for(int i=0; i<n; i++){
+        if(a[i]-1 == lastsmaller){
+            cnt+=1;
+            lastsmaller = a[i];
+        }
+        else if(a[i] != lastsmaller){
+            cnt=1;
+            lastsmaller=a[i];
+        }
+        longest = max(longest,cnt);
+    }
+    return longest;
+}
+
+
+
+//optimal approach 
+int longestSuccessiveElements(vector<int>&a) {
+    // Write your code here.
+    int n= a.size();
+    if (n == 0) return 0;
+   
+    int longest =1;
+    unordered_set <int> st;
+    for(int i=0; i<n; i++){
+        st.insert(a[i]);
+    }
+
+    for(auto it: st){
+        if(st.find(it-1) == st.end()){
+            int cnt=1;
+            int x = it;
+
+            while(st.find(x+1) != st.end()){
+                cnt+=1;
+                x+=1;
+            }
+            longest = max(longest,cnt);
+
+        }
+    }
+    return longest;
+}
